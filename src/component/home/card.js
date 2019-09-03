00b181d0547fbc3eb3c9d5e729b86add1b5a3be8
Shubham Sahu <shusahu@publicisgroupe.net>
2019-09-03 14:55:51 +0530

    changes for responsivness

--- src/component/home/card.js
+++ src/component/home/card.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 import classnames from "classnames";
-import './card.css';
+import "./card.css";
 
-export default (props) => (
-  <div className={classnames({CardWraper:1,revs:props.rowRev})}>
-    <img className="CardImg" src={require(`${props.url}`)}  alt="Smiley face" ></img>
-    <div className="info">
-      <span><strong>{props.heading}</strong></span>
-      <span className="infoText">{props.info}</span>
-      <button className="btn">{props.btnLable}</button>
+export default props => (
+    <div className={classnames({ CardWraper: 1, revs: props.rowRev })}>
+      <img className="CardImg" src={props.url} alt="Smiley face"></img>
+      <div className="info">
+        <span>
+          <strong>{props.heading}</strong>
+        </span>
+        <span className="infoText">{props.info}</span>
+        <button className="btn">{props.btnLable}</button>
+      </div>
     </div>
-  </div>
-)
+  );

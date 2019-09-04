b8097c10f174785edc296320f564c315ffe57881
Shubham Sahu <shusahu@publicisgroupe.net>
2019-09-04 14:21:25 +0530

    impliment media query for mobile in product page

--- src/component/products/porduct.js
+++ src/component/products/porduct.js
@@ -1,33 +1,38 @@
 import React from "react";
-import './product.css';
+import "./product.css";
 
 class Product extends React.Component {
-  state = {
-  };
-  
-  componentDidMount() {
-    
-  }
+  state = {};
+
+  componentDidMount() {}
   render() {
-      const {name,imageURL,description,price} = this.props
+    const { name, imageURL, description, price } = this.props;
     return (
       <div className="productWraper">
-       <div className="heading"><strong>{name}</strong></div>
-       <img className="productImg" src={imageURL} alt="Smiley face"></img>
-       <div className="productInfo">{description}</div>
-       <div className="priceWrapDestop">
-           <div className="productMrp">
-                MRP Rs.{price}
-           </div>
-           <div className="buyNow">
-           BuyNow
-           </div>
-       </div>
-       <div className="priceWrapTab">
-            <div className="buyNow">
-             BuyNow @ Rs.{price}
-           </div>
-       </div>
+        <div className="heading">
+          <strong>{name}</strong>
+        </div>
+
+        <div className="productImgInfo destopTab">
+          <img className="productImg" src={imageURL} alt="Smiley face"></img>
+          <div className="productInfo">{description}</div>
+        </div>
+        <div className="productImgInfo mobile">
+          <img className="productImg" src={imageURL} alt="Smiley face"></img>
+          <div className="productInfoPrice">
+            <div className="productInfo">{description}</div>
+            <div className="priceWrap">
+              <div className="buyNow">BuyNow @ Rs.{price}</div>
+            </div>
+          </div>
+        </div>
+        <div className="priceWrap destop">
+          <div className="productMrp">MRP Rs.{price}</div>
+          <div className="buyNow">BuyNow</div>
+        </div>
+        <div className="priceWrap tab">
+          <div className="buyNow">BuyNow @ Rs.{price}</div>
+        </div>
       </div>
     );
   }

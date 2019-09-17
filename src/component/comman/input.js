734d6b2390d4bf6362904a680667f11b7808c17b
Shubham Sahu <shusahu@publicisgroupe.net>
2019-09-17 14:08:44 +0530

    adding some for tag

--- src/component/comman/input.js
+++ src/component/comman/input.js
@@ -3,16 +3,18 @@ import React from "react";
 export const inputField = ({
   input,
   label,
+  name,
   type,
   meta: { touched, error }
 }) => (
   <div className="inputWraper">
-    <label htmlFor="inp" className="inp">
-      <input {...input} placeholder="&nbsp;" type={type} />
-      <span className="label">{label}</span>
+    <label htmlFor={name} className="inp">
+      <input {...input} id={name} placeholder="&nbsp;" type={type} />
+      <span htmlFor={name} className="label">
+        {label}
+      </span>
       <span className="border"></span>
     </label>
-
     {touched && error && <span>{error}</span>}
   </div>
 );

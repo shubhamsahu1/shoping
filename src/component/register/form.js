e51cc4e1b668df143c8636deae06f55955deb6c7
shubham sahu <shubhamsahu.sahu@gmail.com>
2019-09-17 21:48:14 +0530

    validation for register form

--- src/component/register/form.js
+++ src/component/register/form.js
@@ -7,6 +7,43 @@ const submit = (values, changePage) => {
   console.log(values);
   changePage("/");
 };
+const validate = values => {
+  const errors = {};
+  if (!values.firstname) {
+    errors.firstname = "Required";
+  }
+  if (!values.lastname) {
+    errors.lastname = "Required";
+  }
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  if (!values.password) {
+    errors.password = "Required";
+  } else if (values.password.length < 6) {
+    errors.password = "Minimum length 6 characters";
+  } else if (
+    !/^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/i.test(values.password)
+  ) {
+    errors.password = "Must have a number and alphabet";
+  }
+
+  if (!values.confirmPassword) {
+    errors.confirmPassword = "Required";
+  } else if (values.confirmPassword.length < 6) {
+    errors.confirmPassword = "Minimum length 6 characters";
+  } else if (
+    !/^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/i.test(values.confirmPassword)
+  ) {
+    errors.confirmPassword = "Must have a number and alphabet";
+  } else if (values.confirmPassword !== values.password) {
+    errors.confirmPassword = "Confirm password needs to be same as password";
+  }
+  return errors;
+};
 class Register extends React.Component {
   render() {
     const { handleSubmit, changePage } = this.props;
@@ -52,7 +89,8 @@ class Register extends React.Component {
 
 const registerForm = reduxForm({
   // a unique name for the form
-  form: "register"
+  form: "register",
+  validate
 })(Register);
 
 const mapStateToProps = state => {

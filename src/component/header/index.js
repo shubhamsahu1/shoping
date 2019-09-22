2534e075fc7a4e135f4c47e6e7c077e6fd9a12d8
shubham sahu <shubhamsahu.sahu@gmail.com>
2019-09-23 01:13:37 +0530

    adding event on hambergur icon

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import classnames from "classnames";
 import i18next from "i18next";
@@ -6,62 +6,81 @@ import { bindActionCreators } from "redux";
 import { Link } from "react-router-dom";
 import { toggleCart } from "./../../reducers";
 
-const Header = props => (
-  <header className="wraper">
-    <img
-      src={"/static/images/logo.png"}
-      className="logo"
-      alt="sabka bajaar"
-    ></img>
-    <img src={"/static/menu.png"} className="openMenu" alt="Open Menu"></img>
-    <div className="center">
-      <Link to="/">{i18next.t("Home")}</Link>
-      <Link to="/product">{i18next.t("Product")}</Link>
-    </div>
-    <div className="right">
-      <div className="loginSection">
-        <Link to="/login">{i18next.t("SignIn")}</Link>
-        <Link to="/register">{i18next.t("Register")}</Link>
+const Header = props => {
+  const [hambergur, setHambergur] = useState(false);
+  return (
+    <header className="wraper">
+      <img
+        src={"/static/images/logo.png"}
+        className="logo"
+        alt="sabka bajaar"
+      ></img>
+      <img
+        src={"/static/menu.png"}
+        className="openMenu"
+        alt="Open Menu"
+        onClick={() => setHambergur(!hambergur)}
+      ></img>
+      <div className="center">
+        <Link to="/">{i18next.t("Home")}</Link>
+        <Link to="/product">{i18next.t("Product")}</Link>
       </div>
-      <button className="cart-wraper" onClick={() => props.toggleCart()}>
-        <img src={"/static/images/cart.svg"} alt="cart"></img>
-        <span>
-          {props.totalItem} {i18next.t("items")}
-        </span>
-      </button>
-    </div>
-    <div className={classnames({ hamburger: 1, "hamburger--show": 0 })}>
-      <div className="hamburger-row1">
+      <div className="right">
+        <div className="loginSection">
+          <Link to="/login">{i18next.t("SignIn")}</Link>
+          <Link to="/register">{i18next.t("Register")}</Link>
+        </div>
         <button className="cart-wraper" onClick={() => props.toggleCart()}>
           <img src={"/static/images/cart.svg"} alt="cart"></img>
           <span>
             {props.totalItem} {i18next.t("items")}
           </span>
         </button>
-        <div className="hamburger-row1--close">
-          <img
-            src="/static/baseline_close_black_18dp.png"
-            alt="close hamburger"
-          ></img>
+      </div>
+      <div
+        className={classnames({ hamburger: 1, "hamburger--show": hambergur })}
+      >
+        <div className="hamburger-row1">
+          <button
+            className="cart-wraper"
+            onClick={() => {
+              props.toggleCart();
+            }}
+          >
+            <img src={"/static/images/cart.svg"} alt="cart"></img>
+            <span>
+              {props.totalItem} {i18next.t("items")}
+            </span>
+          </button>
+          <div className="hamburger-row1--close">
+            <img
+              src="/static/baseline_close_black_18dp.png"
+              alt="close hamburger"
+              onClick={() => setHambergur(!hambergur)}
+            ></img>
+          </div>
+        </div>
+        <div
+          className="hamburger-row2"
+          onClick={() => setHambergur(!hambergur)}
+        >
+          <Link className="hamburger-row2--link" to="/">
+            {i18next.t("Home")}
+          </Link>
+          <Link className="hamburger-row2--link" to="/product">
+            {i18next.t("Product")}
+          </Link>
+          <Link className="hamburger-row2--link" to="/login">
+            {i18next.t("SignIn")}
+          </Link>
+          <Link className="hamburger-row2--link" to="/register">
+            {i18next.t("Register")}
+          </Link>
         </div>
       </div>
-      <div className="hamburger-row2">
-        <Link className="hamburger-row2--link" to="/">
-          {i18next.t("Home")}
-        </Link>
-        <Link className="hamburger-row2--link" to="/product">
-          {i18next.t("Product")}
-        </Link>
-        <Link className="hamburger-row2--link" to="/login">
-          {i18next.t("SignIn")}
-        </Link>
-        <Link className="hamburger-row2--link" to="/register">
-          {i18next.t("Register")}
-        </Link>
-      </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 const mapStateToProps = state => ({
   totalItem: state.cart.totalItem
